98723c3af9e1182ed3b2b55d3f793977964c7a59
eugenioclrc <eugenioclrc@gmail.com>
2022-12-06 17:00:51 -0300

    add link to source

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -84,6 +84,10 @@
 			<div class="flex flex-col space-y-4 lg:mt-0 lg:flex-row lg:space-y-0">
 				<a
 					class="text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 lg:mx-6"
+					href="https://github.com/eugenioclrc/rareskill-webtres">source code</a
+				>
+				<a
+					class="text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 lg:mx-6"
 					href="https://webtres.club/">webtres.club/</a
 				>
 				<a
